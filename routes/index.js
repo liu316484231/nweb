d0b3cd36b9e3de849f1b25a8ef10be4a87005fca
admin <bingliu212536@sohu-inc.com>
2017-11-23 17:45:34 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.end('Hello Node');
     res.render("index");
 });
 
+router.get('/tool', function(req, res, next) {
+    res.render("tool");
+});
+
 router.get('/upload', function(req, res, next) {
-    //res.end('Hello Node');
     res.render("upload");
 });
 
